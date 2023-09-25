d8fb8af6f207fda67d9ed2377c82e1b1e8a28bee
Juan Jose <chevike15@gmail.com>
2023-09-25 13:28:08 -0600

    Terminada la seccion de productos populares

---
+++ src/components/ProductosPopularesCard.jsx
@@ -0,0 +1,24 @@
+import { star } from '../assets/icons';
+
+const ProductosPopularesCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img src={star} alt="rating" width={24} height={24} />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">
+          {}
+        </p>
+      </div>
+      <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
+        {name}
+      </h3>
+      <p className="font-montserrat text-2xl leading-normal mt-2 font-semibold text-coral-red">
+        {price}
+      </p>
+    </div>
+  );
+};
+
+export default ProductosPopularesCard;
--- src/sections/ProductosPopulares.jsx
+++ src/sections/ProductosPopulares.jsx
@@ -1,8 +1,31 @@
+import ProductosPopularesCard from '../components/ProductosPopularesCard';
+import { Productos } from '../constants';
 
 const ProductosPopulares = () => {
   return (
-    <div>ProductosPopulares</div>
-  )
-}
+    <section className="max-container max-sm:mt-12" id="Productos">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Nuestros <span className="text-coral-red">Productos</span> Populares
+        </h2>
+        <p className="lg:max-w-lg mt-2 text-slate-gray">
+          Experimente la calidad y el estilo con nuestras selecciones
+          solicitadas. Descubre un mundo de confort, diseño y valor.
+        </p>
+      </div>
 
-export default ProductosPopulares
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {Productos.map((producto) => (
+          <ProductosPopularesCard
+            key={producto}
+            imgURL={producto.imgURL}
+            name={producto.name}
+            price={producto.price}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProductosPopulares;
