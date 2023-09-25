00a205deef5d271df4b60f8da93d86508a23a07e
Juan Jose <chevike15@gmail.com>
2023-09-24 21:02:52 -0600

    Iniciando el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline m-20 bg-red-500 text-center rounded-lg cursor-pointer text-green-400 hover:bg-red-700 hover:text-white">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
